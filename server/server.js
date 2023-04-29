feb7efc14a3e766a2ca6b3ed7090537280c3b451
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2023-04-29 19:36:26 +0200

    main path change

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use('/api', indexRouter)
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 })
 
