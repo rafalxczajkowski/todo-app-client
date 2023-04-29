476298089c0b291310c3cac23de3e7ea9753be55
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2023-04-29 19:00:30 +0200

    changes needed to Heroku

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
@@ -13,6 +14,11 @@ const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use('/api', indexRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
 
 app.listen(PORT, console.log(`Server is running on port: ${PORT}`))
