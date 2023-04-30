e291d5c62def25a9e906dc63cf79bbde3266221c
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2023-04-30 20:09:21 +0200

    proxy change

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
-app.use(express.json())
 app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.json())
 app.use('/api', indexRouter)
 
 app.get('*', (req, res) => {
