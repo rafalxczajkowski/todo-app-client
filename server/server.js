67082fc8729fae71d5c1bab9c22c649ff76fa8ef
Rafal Czajkowski <rafalxczajkowski@hotmail.com>
2023-10-10 13:30:16 +0200

    cleaning

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
@@ -8,8 +6,6 @@ import router from './routes/index.js'
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 3001
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 connect(process.env.MONGO_URI, console.log('Connected to database'))
 
@@ -17,12 +13,7 @@ const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(express.json())
 app.use('/api', router)
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-})
 
 app.listen(PORT, console.log(`Server is running on port: ${PORT}`))
